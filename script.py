a0a2dbab2683742d716cdd6ba6dccd7d4ce69e63
Ivan Fernandez <100383564@alumnos.uc3m.es>
2020-10-06 19:07:56 +0200

    Reset index

--- script.py
+++ script.py
@@ -13,6 +13,10 @@ for column in data.columns:
 training, testing = train_test_split(data, shuffle=True, test_size=0.4)
 testing, validation = train_test_split(testing, shuffle=True, test_size=0.5)
 
+training = training.reset_index(drop=True)
+testing = testing.reset_index(drop=True)
+validation = validation.reset_index(drop=True)
+
 print(training)
 print(testing)
 print(validation)
