36702b4afde42dd5118b41c9a89c454001669039
Ivan Fernandez <100383564@alumnos.uc3m.es>
2020-10-06 18:35:26 +0200

    Test

--- script.py
+++ script.py
@@ -2,4 +2,11 @@ import pandas as pd
 houses = pd.read_csv("california_housing.csv")
 
 for column in houses.columns:
-    print(houses[column].max())
+    max = houses[column].max()
+    min = houses[column].min()
+    minus = (max-min)
+    for index, value in enumerate(houses[column]):
+        if index < 3:
+            houses[column][index] = (value-min)/minus
+            print(houses[column][index])
+    
