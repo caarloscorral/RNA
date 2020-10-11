2f1e5e9d2e4fc2c7565f42a7a2e8761ee0597b3a
Ivan Fernandez <100383564@alumnos.uc3m.es>
2020-10-11 15:25:18 +0200

    changes

--- script.py
+++ script.py
@@ -1,21 +1,21 @@
 # Importing libraries
-import pandas as pd
-import numpy as np
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
 import random
 
-############################################################# PREPROCESSING #############################################################
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+################################## PREPROCESSING ###################################
 
 # Getting data
 data = pd.read_csv("california_housing.csv")
 
 # Normalize data
 for column in data.columns:
-    max = data[column].max()
-    min = data[column].min()
-    minus = (max-min)
-    data[column] -= min
+    maxx = data[column].max()
+    minn = data[column].min()
+    minus = (maxx-minn)
+    data[column] -= minn
     data[column] /= minus
 
 # Getting random training, testing and validation sets
@@ -36,7 +36,6 @@ validation = validation.reset_index(drop=True)
 
 ##### Variables ####
 learning_rate = 0.3
-threshold = 0
 
 def Adaline_Training(cycles):
     ''' weights[0] = w1       weights[1] = w2       weights[2] = w3       weights[3] = w4
@@ -46,13 +45,13 @@ def Adaline_Training(cycles):
     # Inizializing random weights and threshold
     weights = np.around(np.random.rand(1, 8), 3)
 
-    threshold = round(random.random(), 3)
+    threshold = random.random() / 1000.0
 
     mse_training_list = list()
     mse_validation_list = list()
 
     # Training Adaline by the number of cycles (stop criterion)
-    for cycle in range(cycles):
+    for _ in range(cycles):
         training_errors = list()
         validation_errors = list()
 
@@ -61,7 +60,7 @@ def Adaline_Training(cycles):
         for pattern in training.values:
             # Getting output
             output = weights @ pattern[0:8]
-            output += threshold
+            # output += threshold
 
             difference = pattern[8] - output
 
@@ -74,35 +73,38 @@ def Adaline_Training(cycles):
             # Appending pattern error to training errors list
             training_errors.append(difference)
 
+        tError = np.array(training_errors)
+
         # For each cycle get Mean Squared Error (MSE) for training set
-        mse_training = 0
-        for error in training_errors:
-            mse_training += error ** 2
-        mse_training /= len(training.values)
+        mse_training = np.sum(tError ** 2)
+        mse_training /= len(tError)
         mse_training_list.append(mse_training)
 
 
         ##### VALIDATION ####
         for pattern in validation.values:
-           # Getting output
-            output = 0
-            for x in range(len(pattern) - 1):
-                output += weights[x] * pattern[x]
-            output += threshold
+            #Getting output
+            output = weights @ pattern[0:8]
+            # output += threshold
 
             difference = pattern[8] - output
 
             # Appending pattern error to validation errors list
             validation_errors.append(difference)
 
+        vError = np.array(validation_errors)
+
         # For each cycle get Mean Squared Error (MSE) for validation set
-        mse_validation = 0
-        for error in validation_errors:
-            mse_validation += error ** 2
+        mse_validation = np.sum(vError ** 2)
         mse_validation /= len(validation.values)
         mse_validation_list.append(mse_validation)
-        
+
+        if mse_training < threshold:
+            #break
+            pass
+    
     # Printing into screen
+    print(mse_training_list)
 
 
 def main():
