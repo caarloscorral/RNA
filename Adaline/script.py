e5b4fe6405ea42f56f616b75f225d811595fadbf
Carlos Corral <carlos.gcorral@hotmail.com>
2020-10-13 18:34:53 +0200

    Final

--- Adaline/script.py
+++ Adaline/script.py
@@ -66,7 +66,7 @@ def Adaline_Training(cycles, learning_rate, training):
 
             # Setting new weights
             weights[0:8] += learning_rate * difference * pattern[0:8]
-            
+
             # Setting new threshold
             weights[8] += learning_rate * difference
 
@@ -80,10 +80,9 @@ def Adaline_Training(cycles, learning_rate, training):
         mse_training /= len(tError)
         mse_training_list.append(mse_training)
 
-
         ##### VALIDATION ####
         for i in range(len(validation.values)):
-            #Getting output
+            # Getting output
             output = weights[0:8] @ validation.values[i][0:8]
             output += weights[8]
 
@@ -104,11 +103,13 @@ def Adaline_Training(cycles, learning_rate, training):
 
     # Printing into screen the errors table and saving into a dataframe
     error_table = list(zip(mse_training_list, mse_validation_list))
-    error_table = pd.DataFrame(error_table, columns=["Error medio de entrenamiento", "Error medio de validación"])
+    error_table = pd.DataFrame(error_table, columns=[
+                               "Error medio de entrenamiento", "Error medio de validación"])
     error_table.reset_index(inplace=True)
-    error_table = error_table.rename(columns = {'index': 'Ciclo'})
+    error_table = error_table.rename(columns={'index': 'Ciclo'})
 
-    print("Learning rate: " + str(learning_rate) + "\n" + error_table.to_string(index=False) + "\n********************************************************************************************")
+    print("Learning rate: " + str(learning_rate) + "\n" + error_table.to_string(index=False) +
+          "\n********************************************************************************************")
 
     # Saving outputs into a dataframe
     outputs_table = pd.DataFrame(outputs)
@@ -116,7 +117,8 @@ def Adaline_Training(cycles, learning_rate, training):
     # Saving final weights and threshold into a dataframe
     weights_and_threshold_table = pd.DataFrame(weights)
     weights_and_threshold_table = weights_and_threshold_table.T
-    weights_and_threshold_table.columns = ["Peso final w1", "Peso final w2", "Peso final w3", "Peso final w4", "Peso final w5", "Peso final w6", "Peso final w7", "Peso final w8", "Umbral final"]
+    weights_and_threshold_table.columns = ["Peso final w1", "Peso final w2", "Peso final w3",
+                                           "Peso final w4", "Peso final w5", "Peso final w6", "Peso final w7", "Peso final w8", "Umbral final"]
 
     # Saving errors table, weights and threshold table, and outputs table into Excel adn CSV files; checking first if there are existent files and removing them
     if os.path.exists(r"Adaline\Outputs\Adaline" + str(learning_rate) + ".xlsx"):
@@ -126,56 +128,129 @@ def Adaline_Training(cycles, learning_rate, training):
         os.remove(r"Adaline\Outputs\Errors" + str(learning_rate) + ".csv")
 
     elif os.path.exists(r"Adaline\Outputs\Final weights"+str(learning_rate) + ".csv"):
-        os.remove(r"Adaline\Outputs\Final weights" + str(learning_rate) + ".csv")
+        os.remove(r"Adaline\Outputs\Final weights" +
+                  str(learning_rate) + ".csv")
 
     elif os.path.exists(r"Adaline\Outputs\Outputs" + str(learning_rate) + ".csv"):
         os.remove(r"Adaline\Outputs\Outputs" + str(learning_rate) + ".csv")
 
-    writer = pd.ExcelWriter(r"Adaline\Outputs\Adaline" + str(learning_rate) + ".xlsx", engine = 'xlsxwriter')
+    writer = pd.ExcelWriter(r"Adaline\Outputs\Adaline" +
+                            str(learning_rate) + ".xlsx", engine='xlsxwriter')
 
     error_table.to_excel(writer, sheet_name="Errors", index=False, header=True)
-    error_table.to_csv(r"Adaline\Outputs\Errors" + str(learning_rate) + ".csv", index=False)
+    error_table.to_csv(r"Adaline\Outputs\Errors" +
+                       str(learning_rate) + ".csv", index=False)
 
-    weights_and_threshold_table.to_excel(writer, sheet_name="Final weights", index=False, header=True)
-    weights_and_threshold_table.to_csv(r"Adaline\Outputs\Weights_and_threshold" + str(learning_rate) + ".csv", index=False)
+    weights_and_threshold_table.to_excel(
+        writer, sheet_name="Final weights", index=False, header=True)
+    weights_and_threshold_table.to_csv(
+        r"Adaline\Outputs\Weights_and_threshold" + str(learning_rate) + ".csv", index=False)
 
     # Denormalize outputs
     for column in outputs_table.columns:
         minus = (maxx["median_house_value"] - minn["median_house_value"])
         outputs_table[column] *= minus
         outputs_table[column] += minn["median_house_value"]
+        outputs_table[column] = np.around(outputs_table[column], 2)
 
     outputs_table.to_excel(writer, sheet_name="Outputs", header=True)
-    outputs_table.to_csv(r"Adaline\Outputs\Outputs" + str(learning_rate) + ".csv", index=False)
+    outputs_table.to_csv(r"Adaline\Outputs\Outputs" +
+                         str(learning_rate) + ".csv", index=False)
 
     writer.save()
     writer.close()
 
-    return mse_validation_list
+    return mse_validation_list, weights
+
+
+def testing_error(weights):
+    testing_errors = []
+    testing_outputs = []
+
+    for pattern in testing.values:
+        # Getting output
+        output = weights[0:8] @ pattern[0:8]
+        output += weights[8]
+
+        difference = pattern[8] - output
+
+        # Appending pattern error to testing errors list
+        testing_errors.append(difference)
+
+        # Saving outputs into outputs list
+        testing_outputs.append(output)
+
+    # For each cycle get Mean Squared Error (MSE) for testing set
+    testing_errors = np.array(testing_errors)
+    mse_testing = np.sum(testing_errors ** 2)
+    mse_testing /= len(testing_errors)
+
+    # Denormalize outputs
+    for index, output in enumerate(testing_outputs):
+        minus = (maxx["median_house_value"] - minn["median_house_value"])
+        output *= minus
+        output += minn["median_house_value"]
+        testing_outputs[index] = np.around(output, 2)
+
+    # Saving final weights and threshold into a dataframe
+    weights = np.append(weights, mse_testing)
+    weights_and_threshold_table = pd.DataFrame(weights)
+    weights_and_threshold_table = weights_and_threshold_table.T
+    weights_and_threshold_table.columns = ["Peso final w1", "Peso final w2", "Peso final w3",
+                                           "Peso final w4", "Peso final w5", "Peso final w6", "Peso final w7", "Peso final w8", "Umbral final", "MSE Testing Final"]
+    
+    # Saving final outputs into a dataframe
+    testing_outputs_table = pd.DataFrame(testing_outputs)
+
+    return weights_and_threshold_table, testing_outputs_table
+
 
 def main():
     MAX_CYCLES = 100
     errors = []
     cycles = []
-    learning_rates = [0.001, 0.005, 0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 1]
+    weight_list = []
+    learning_rates = [0.001, 0.01]
 
     for rate in learning_rates:
-        error = Adaline_Training(MAX_CYCLES, rate, training_set)
+        error, weights = Adaline_Training(MAX_CYCLES, rate, training_set)
         newCycles = error.index(min(error)) + 1
         cycles.append(newCycles)
-        bestError = Adaline_Training(newCycles, rate, training_set)
+        bestError, weights = Adaline_Training(newCycles, rate, training_set)
         errors.append(min(bestError))
+        weight_list.append(weights)
 
+    best_learning_rate_index = errors.index(min(errors))
+    best_learning_rate = learning_rates[best_learning_rate_index]
+    weights_and_threshold_table, testing_outputs_table = testing_error(weight_list[best_learning_rate_index])
+
+    # Saving errors testing table, weights and threshold table, and outputs table into Excel adn CSV files; checking first if there are existent files and removing them
+    if os.path.exists(r"Adaline\Outputs\FinalWeights.csv"):
+        os.remove(r"Adaline\Outputs\FinalWeights.csv")
+
+    elif os.path.exists(r"Adaline\Outputs\FinalOutputs.csv"):
+        os.remove(r"Adaline\Outputs\FinalOutputs.csv")
+
+    writer = pd.ExcelWriter(r"Adaline\Outputs\FinalWeights.xlsx", engine='xlsxwriter')
+    weights_and_threshold_table.to_excel(writer, index=False, header=True)
+    weights_and_threshold_table.to_csv(r"Adaline\Outputs\FinalWeights.csv", index=False)
+
+    writer = pd.ExcelWriter(r"Adaline\Outputs\FinalOutputs.xlsx", engine='xlsxwriter')
+    testing_outputs_table.to_excel(writer, index=False, header=True)
+    testing_outputs_table.to_csv(r"Adaline\Outputs\FinalOutputs.csv", index=False)
+
+    writer.save()
+    writer.close()
 
     if os.path.exists(r"Adaline\Outputs\Errors.csv"):
         os.remove(r"Adaline\Outputs\Errors.csv")
 
     error_table = list(zip(learning_rates, cycles, errors))
-    error_table = pd.DataFrame(error_table, columns=["Ratio de aprendizaje", "Ciclos óptimos", "Error de validación"])
+    error_table = pd.DataFrame(error_table, columns=[
+                               "Ratio de aprendizaje", "Ciclos óptimos", "Error de validación"])
     error_table.reset_index(drop=True)
     error_table.to_csv(r"Adaline\Outputs\Errors.csv", index=False)
 
 
-    
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
